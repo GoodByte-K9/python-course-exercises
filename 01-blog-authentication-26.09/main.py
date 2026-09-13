e18a61d2bb999d21bc063017bef8539c2b97d3b9
Good Byte <GoodByte-K9@proton.me>
2026-09-13 16:25:56 +0300

    Add Procfile, gunicorn, and PostgreSQL config

--- 01-blog-authentication-26.09/main.py
+++ 01-blog-authentication-26.09/main.py
@@ -5,9 +5,9 @@ from extensions import app, db, login_manager
 import routes
 
 load_dotenv()
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI", "sqlite:///posts.db")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db.init_app(app)
 login_manager.init_app(app)
 
@@ -15,4 +15,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
